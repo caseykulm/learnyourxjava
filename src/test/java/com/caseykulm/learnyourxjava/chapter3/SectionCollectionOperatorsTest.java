8fbe189b2aafa00f5527edf3fe6e9818f8b25228
Casey Kulm <mlukyesac@gmail.com>
2018-03-13 21:01:29 -0600

    Finish collection operators

--- src/test/java/com/caseykulm/learnyourxjava/chapter3/SectionCollectionOperatorsTest.java
+++ src/test/java/com/caseykulm/learnyourxjava/chapter3/SectionCollectionOperatorsTest.java
@@ -1,6 +1,7 @@
 package com.caseykulm.learnyourxjava.chapter3;
 
 import java.util.Comparator;
+import java.util.HashSet;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,6 +26,31 @@ public class SectionCollectionOperatorsTest {
   public void toSortedListExample() throws Exception {
     subj.words
         .toSortedList(Comparator.comparingInt(String::length))
+        .subscribe(System.out::println);
+  }
+
+  @Test
+  public void toMapExample() throws Exception {
+    subj.words
+        .toMap(word -> word.charAt(0))
+        .subscribe(System.out::println);
+
+    subj.words
+        .toMap(word -> word.charAt(0), String::length)
+        .subscribe(System.out::println);
+  }
+
+  @Test
+  public void toMultiMapExample() throws Exception {
+    subj.words
+        .toMultimap(String::length)
+        .subscribe(System.out::println);
+  }
+
+  @Test
+  public void collectExample() throws Exception {
+    subj.words
+        .collect(HashSet::new, HashSet::add)
         .subscribe(System.out::println);
   }
 }
